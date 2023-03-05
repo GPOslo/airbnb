df5f9c0c0d477d75b51b7a94f058c2df4fc82013
Jaehyeon <112854083+jaehyeonkim2358@users.noreply.github.com>
2023-03-05 13:28:49 +0900

    [INIT] backend initial setup

---
+++ backend/airbnb-clone/src/main/java/com/GPOslo/airbnbclone/AirbnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.GPOslo.airbnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneApplication.class, args);
+	}
+
+}
