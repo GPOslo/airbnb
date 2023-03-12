5c3a4853d0ed75e4243ffe2b21235cfc7c4deab1
narin <nalynnshinhwa1128@gmail.com>
2023-03-12 23:13:25 +0900

    [FEAT] 스프링부트와 연동을 위한 proxy 설정

---
+++ frontend/src/setProxy.js
@@ -0,0 +1,11 @@
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//     app.use(
+//         "/auth",
+//         createProxyMiddleware({
+//             target: "https://aibnbclone.site",
+//             changeOrigin: true,
+//         })
+//     );
+// };
