0adbca46536f5f5ffe3a07f73ff251440511bae4
narin <nalynnshinhwa1128@gmail.com>
2023-03-12 23:18:43 +0900

    [CHORE] package dependencies 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 function App() {
-    return;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
